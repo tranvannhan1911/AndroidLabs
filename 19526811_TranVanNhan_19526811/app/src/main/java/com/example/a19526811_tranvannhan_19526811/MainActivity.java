6715cba5c56b08418a24b2eaf29c8f24ce93f88d
tranvannhan1911 <tranvannhan1911@gmail.com>
2022-04-21 21:16:18 +0700

    TH9

---
+++ 19526811_TranVanNhan_19526811/app/src/main/java/com/example/a19526811_tranvannhan_19526811/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.a19526811_tranvannhan_19526811;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSignIn = findViewById(R.id.btn_sign_in);
+        btnSignIn.setOnClickListener((v) -> {
+            startActivity(new Intent(this, SignInActivity.class));
+        });
+
+        Button btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener((v) -> {
+            startActivity(new Intent(this, RegisterActivity.class));
+        });
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+}
---
+++ 19526811_TranVanNhan_19526811/app/src/main/java/com/example/a19526811_tranvannhan_19526811/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.a19526811_tranvannhan_19526811;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
